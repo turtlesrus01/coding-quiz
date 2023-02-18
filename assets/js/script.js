9dd0159fa06ab32e0e2385613b4d8e62b0f76875
Green Camper <stefan.palacios@gmail.com>
2023-02-17 23:10:18 -0600

    Wrote psuedocode for the variety of states the coding quiz will cover

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+//When starting the quiz, it must be started when the 'Start Quiz button is clicked
+
+//Function fpr counting down when a new question is shown
+//Function for handling when a choice is clicked
+//--Once choice is clicked you must check if the question is correct
+//--If answer is correct it must move to the next question
+//--If incorrect it must say Wrong! at the bottom of the page
+//Once a question is completed the page must go to the next question
+//After several questions are completed the end of the quiz will say 
+//--'All done!' 
+//--Show your final score 
+//--You can enter you initials  
+//--Then, print the top scores like a leaderboard
+//--This data for scores will be stored in local memory
+//--You must also be able to clear the scoreboard
