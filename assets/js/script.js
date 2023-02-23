5530cd56fe0d9dc8bea3156c78778ba8e7bc6c8b
Green Camper <stefan.palacios@gmail.com>
2023-02-22 18:46:26 -0600

    Created timer function in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,8 @@ var quizArea = document.getElementById('quiz');
 var submitBtn = document.getElementById('#submitBtn');
 var showAnswer = document.getElementById('showAnswer');
 var startQuizBtn = document.querySelector('startQuiz');
+var timerHTML = document.querySelector('timercount')
+//Default state for start quiz button
 var isStartQuizBtn = 'hidden';
 
 var questions = [
@@ -75,8 +77,11 @@ function showQuestion(){
   //Pull and print question piece by piece
   //Show Answers
   //We can access the questions array using dot notation
+  //This stores the answer choices
   var showChoices = [];
   var answer = [];
+  //This stores the timer at default time, 100 seconds
+  var timerCount = 100;
 
   //stores check against answer that is picked
   var questionCount = 4;
@@ -102,19 +107,38 @@ function showQuestion(){
 
   };
   console.log(show);
+
+  //timer function counts down from 100 seconds and decrements when an incorrect answer is given
+    function timer(){
+      
+      //Time count is stored in this variable
+      var timeInterval = setInterval(function(){
+        timerCount--;
+        
+        if (timerCount === 0){
+          //Clears timer count 
+          clearInterval(timeInterval)
+          //Prints to page when time runs out
+          alert('You have run out of time')
+        } 
+      }, 1000);
+  }
+  //Then call to start countdown
+  timer();
+  //Returns text to the quiz area after quiz is built
   quizArea.innerText = answer;
-  
 
-}
+};
 
+//An alert to the user that their answer is correct or incorrect
 function showAnswer(event) {
   if(correctAnswer.check){
     alert("Correct")
   }
-}
+};
 
 
 //When user clicks submit answer button, the function will verify the choice
 //submitBtn.addEventListener('click', showAnswer);
 //When user clicks start quiz, 'submit answer' button is unhidden, timer starts and question is built 
-startQuiz.addEventListener('click', showQuestion);
+startQuizBtn.addEventListener('click', showQuestion);
